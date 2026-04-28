bb13510e847bfb8a38996ac8701029346de608da
Vitali Shulha <grzegorz.swierk01@gmail.com>
2026-04-28 12:31:17 +0200

    update

--- PracricingBasics/Program.cs
+++ PracricingBasics/Program.cs
@@ -22,26 +22,35 @@ var words = new Dictionary<int, string>()
 };
 */// comment for not making mess but not deleting this in case i want to remind the logic
 
-Console.WriteLine("Choose a drink:");
-Console.WriteLine("8 - Cola");
-Console.WriteLine("22424 - Water");
-Console.WriteLine("399999999 - Juice");
-
-string input = Console.ReadLine();
-int choice = int.Parse(input);
-
-switch (choice)
+namespace PracricingBasics
 {
-    case 8:
-        Console.WriteLine("You chose Cola");
-        break;
-    case 22424:
-        Console.WriteLine("You chose Water");
-        break;
-    case 399999999:
-        Console.WriteLine("You chose Juice");
-        break;
-    default:
-        Console.WriteLine("Invalid choice");
-        break;
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a drink:");
+            Console.WriteLine("8 - Cola");
+            Console.WriteLine("22424 - Water");
+            Console.WriteLine("399999999 - Juice");
+
+            string input = Console.ReadLine();
+            int choice = int.Parse(input);
+
+            switch (choice)
+            {
+                case 8:
+                    Console.WriteLine("You chose Cola");
+                    break;
+                case 22424:
+                    Console.WriteLine("You chose Water");
+                    break;
+                case 399999999:
+                    Console.WriteLine("You chose Juice");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+    }
 }
